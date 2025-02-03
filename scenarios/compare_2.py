593a3bda0343c8ab5f67989e554c893ddde09954
Arseniy Terekhin <senyai@gmail.com>
2025-02-03 14:12:51 +0300

    fix: new seed logic, add new test

--- scenarios/compare_2.py
+++ scenarios/compare_2.py
@@ -8,13 +8,14 @@ from flask import Response, send_from_directory
 
 class Frame(TypedDict):
     path: str
-    choices: list[str]
+    choices: NotRequired[list[str]]
 
 
 class SingleTest(TypedDict):
     start_pause_ms: NotRequired[float]
     frames: list[Frame]
     choices: list[str]
+    gap: NotRequired[str]  # list "100px on 8em"
 
 
 class Test1:
@@ -28,7 +29,8 @@ class Test1:
         all_image_paths = [
             path.name  # make it json serializable
             for path in all_paths
-            if path.is_file() and path.suffix.lower() in (".jpg", ".jpeg", ".png")
+            if path.is_file()
+            and path.suffix.lower() in (".jpg", ".jpeg", ".png")
         ]
         all_image_paths.sort()
         self._items = list(combinations(all_image_paths, 2))
@@ -37,17 +39,57 @@ class Test1:
         print("serving", path, "from", self._root)
         return send_from_directory(self._root, path)
 
-    def item_for_user(self, username: str, idx: int) -> SingleTest:
-        paths = Random(username).sample(self._items, k=5)[idx]
+    def item_for_user(self, seed: int, idx: int) -> SingleTest:
+        paths = Random(seed).sample(self._items, k=5)[idx]
         ret: SingleTest = {
             "start_pause_ms": 1500,
             "frames": [{"path": path} for path in paths],
             "choices": [
                 "left",
                 "right",
-                "all good",
-                "all bad",
                 "unknown",
+            ],
+        }
+        return ret
+
+
+class Test2:
+    """
+    Информация о тесте 2.
+    """
+
+    def __init__(self) -> None:
+        self._root = Path("/home/senyai/Downloads/Telegram Desktop/dataset_1")
+        all_test_paths = [
+            subpath
+            for path in self._root.iterdir()
+            if path.is_dir()
+            for subpath in path.iterdir()
+            if subpath.is_dir()
+        ]
+        all_test_paths.sort()
+        self._items = list(
+            paths
+            for test_path in all_test_paths
+            for paths in combinations(
+                [p.relative_to(self._root) for p in test_path.glob("*.png")], 2
+            )
+        )
+
+    def file(self, path: str) -> Response:
+        # print("serving", path, "from", self._root)
+        return send_from_directory(self._root, path)
+
+    def item_for_user(self, seed: int, idx: int) -> SingleTest:
+        paths = Random(seed).sample(self._items, k=5)[idx]
+        ret: SingleTest = {
+            "start_pause_ms": 1500,
+            "frames": [
+                {"path": str(paths[0]), "choices": ["Left"]},
+                {"path": str(paths[1]), "choices": ["Right"]},
+            ],
+            "choices": [
+                "Unknown",
             ],
         }
         return ret
