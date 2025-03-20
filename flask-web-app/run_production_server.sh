8ba7de9f54bfcfe00f857b8f13e978066fa348af
Arseniy Terekhin <senyai@gmail.com>
2025-03-20 14:01:23 +0300

    fix: add timeout to run_production_server.sh

--- flask-web-app/run_production_server.sh
+++ flask-web-app/run_production_server.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-gunicorn --workers 1 --bind 0.0.0.0:9999 pyolimp_human_studies:app
+gunicorn \
+  --workers 1 \
+  --timeout 120 \
+  --log-level debug \
+  --bind 0.0.0.0:9999 \
+  pyolimp_human_studies:app
